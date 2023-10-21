e904b36b325acf5b14c2bdf6d9135f89450120db
RealKingTino <tmowizzy@gmail.com>
2023-10-21 02:46:29 -0700

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -32,9 +32,9 @@ def display_c(text):
     return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
+def display_python(text="is cool"):
     """
     Route that displays Python
     """
